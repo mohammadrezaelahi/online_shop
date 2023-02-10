657a4bf2a0187bfea1fcc4623ce39b690ce35843
mohammadrezaelahi <m.reza.elahi.1378@gmail.com>
2023-02-10 15:11:22 +0330

    set something for django-allauth

--- config/settings.py
+++ config/settings.py
@@ -27,6 +27,14 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+AUTHENTICATION_BACKENDS = [
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
+
 
 # Application definition
 
@@ -46,6 +54,8 @@ INSTALLED_APPS = [
     "accounts",
     "pages",
 ]
+
+SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
