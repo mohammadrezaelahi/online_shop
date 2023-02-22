987f13d13c5825c31795ac2666a331466c8589c4
mohammadrezaelahi <m.reza.elahi.1378@gmail.com>
2023-02-22 23:21:22 +0330

    change detail view for comments

--- products/views.py
+++ products/views.py
@@ -1,6 +1,8 @@
 from django.views import generic
+from django.shortcuts import get_object_or_404, redirect, render
 
 from .models import Product
+from .forms import CommentForm
 
 
 class ProductListView(generic.ListView):
@@ -9,7 +11,26 @@ class ProductListView(generic.ListView):
     context_object_name = "products"
 
 
-class ProductDetailView(generic.DetailView):
-    model = Product
-    template_name = "products/product_detail.html"
-    context_object_name = "product"
+# class ProductDetailView(generic.DetailView):
+#     model = Product
+#     template_name = "products/product_detail.html"
+#     context_object_name = "product"
+
+def product_detail_view(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    comments = product.comments.all().order_by("-datetime_created")
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            new_comment = form.save(commit=False)
+            new_comment.product = product
+            new_comment.user = request.user
+            new_comment.save()
+            form = CommentForm()
+            return redirect("product_list")
+    else:
+        form = CommentForm()
+
+    return render(request, "products/product_detail.html", {"product": product,
+                                                            "comments": comments,
+                                                            "form": form})
