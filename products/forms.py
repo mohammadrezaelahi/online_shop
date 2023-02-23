7ef1370ef7b3d9bd23605df3b34e949408690297
mohammadrezaelahi <m.reza.elahi.1378@gmail.com>
2023-02-23 17:54:00 +0330

    create CommentForm

--- products/forms.py
+++ products/forms.py
@@ -6,4 +6,8 @@ from .models import Comment
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
-        fields = ("text", )
+        fields = ("body", "stars")
+        labels = {
+            "body": "متن کامنت",
+            "stars": "امتیاز",
+        }
