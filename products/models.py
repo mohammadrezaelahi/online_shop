27fad80c89cd60399d7f5a32e7b22874025c7b00
mohammadrezaelahi <m.reza.elahi.1378@gmail.com>
2023-02-23 17:54:14 +0330

    create model Comment

--- products/models.py
+++ products/models.py
@@ -20,8 +20,25 @@ class Product(models.Model):
 
 
 class Comment(models.Model):
-    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    PRODUCT_STARS = [
+        ("1", "very bad"),
+        ("2", "bad"),
+        ("3", "normal"),
+        ("4", "good"),
+        ("5", "very good"),
+    ]
+    author = models.ForeignKey(get_user_model(), related_name="comments", on_delete=models.CASCADE)
     product = models.ForeignKey(Product, related_name="comments", on_delete=models.CASCADE)
-    text = models.TextField()
+    body = models.TextField()
+    stars = models.CharField(max_length=10, choices=PRODUCT_STARS)
 
     datetime_created = models.DateTimeField(auto_now_add=True)
+    datetime_modified = models.DateTimeField(auto_now=True)
+
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.author} : {self.product}"
+
+    def get_absolute_url(self):
+        return reverse("product_detail", args=[self.product.id])
