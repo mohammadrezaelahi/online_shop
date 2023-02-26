bd81b60e49b9e68d0754472b27ac3e33536ffb5a
mohammadrezaelahi <m.reza.elahi.1378@gmail.com>
2023-02-27 00:01:16 +0330

    add CommentCreatView and test_view in urls

--- products/urls.py
+++ products/urls.py
@@ -5,5 +5,7 @@ from . import views
 
 urlpatterns = [
     path("", views.ProductListView.as_view(), name="product_list"),
-    path("<int:pk>", views.product_detail_view, name="product_detail"),
+    path("<int:pk>/", views.ProductDetailView.as_view(), name="product_detail"),
+    path("comment/<int:product_id>/", views.CommentCreateView.as_view(), name="comment_create"),
+    path("test/", views.test_view, )
 ]
