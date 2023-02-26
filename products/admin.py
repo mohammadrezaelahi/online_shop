ea342fa8b79b4426f42c646818a39d4a4356a4c7
mohammadrezaelahi <m.reza.elahi.1378@gmail.com>
2023-02-26 23:57:03 +0330

    add inlines

--- products/admin.py
+++ products/admin.py
@@ -3,11 +3,20 @@ from django.contrib import admin
 from .models import Product, Comment
 
 
+class CommentsInline(admin.TabularInline):
+    model = Comment
+    fields = ["product", "active", "stars", "body", ]
+    extra = 0
+
+
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ["title", "price", "active"]
+    inlines = [
+        CommentsInline,
+    ]
 
 
 @admin.register(Comment)
-class ProductAdmin(admin.ModelAdmin):
-    list_display = ["author", "product", "active", "body"]
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ["author", "product", "active", "stars", "body", ]
