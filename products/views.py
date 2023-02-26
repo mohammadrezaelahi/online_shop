1f5e090b5825763793ad46bd2260e19c7b769b2b
mohammadrezaelahi <m.reza.elahi.1378@gmail.com>
2023-02-27 00:00:07 +0330

    create test view and CommentCreateView

--- products/views.py
+++ products/views.py
@@ -1,8 +1,17 @@
 from django.views import generic
 from django.shortcuts import get_object_or_404, redirect, render
+from django.utils.translation import gettext as _
+from django.contrib import messages
 
-from .models import Product
+from .models import Product, Comment
 from .forms import CommentForm
+
+
+def test_view(request):
+    result = _("Hello")
+    messages.success(request, _("this is success message."))
+    messages.error(request, _("this is danger message."))
+    return render(request, "products/test.html", {"result": result})
 
 
 class ProductListView(generic.ListView):
@@ -11,26 +20,52 @@ class ProductListView(generic.ListView):
     context_object_name = "products"
 
 
-# class ProductDetailView(generic.DetailView):
-#     model = Product
-#     template_name = "products/product_detail.html"
-#     context_object_name = "product"
+class ProductDetailView(generic.DetailView):
+    model = Product
+    template_name = "products/product_detail.html"
+    context_object_name = "product"
 
-def product_detail_view(request, pk):
-    product = get_object_or_404(Product, pk=pk)
-    comments = product.comments.all().order_by("-datetime_created")
-    if request.method == "POST":
-        form = CommentForm(request.POST)
-        if form.is_valid():
-            new_comment = form.save(commit=False)
-            new_comment.product = product
-            new_comment.user = request.user
-            new_comment.save()
-            form = CommentForm()
-            return redirect("product_list")
-    else:
-        form = CommentForm()
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["comment_form"] = CommentForm()
+        # product_id = int(self.kwargs["pk"])
+        # product = get_object_or_404(Product, pk=product_id)
+        # fee = product.price + ((product.price * 10) / 100)
+        # context["fee"] = fee
+        return context
 
-    return render(request, "products/product_detail.html", {"product": product,
-                                                            "comments": comments,
-                                                            "form": form})
+
+class CommentCreateView(generic.CreateView):
+    model = Comment
+    form_class = CommentForm
+
+    def form_valid(self, form):
+        obj = form.save(commit=False)
+        obj.author = self.request.user
+
+        product_id = int(self.kwargs["product_id"])
+        product = get_object_or_404(Product, pk=product_id)
+        obj.product = product
+        return super().form_valid(form)
+
+
+# def product_detail_view(request, pk):
+#     product = get_object_or_404(Product, pk=pk)
+#     fee = product.price + ((product.price*10) / 100)
+#     comments = product.comments.all().filter(active=True).order_by("-datetime_created")
+#     if request.method == "POST":
+#         form = CommentForm(request.POST)
+#         if form.is_valid():
+#             new_comment = form.save(commit=False)
+#             new_comment.product = product
+#             new_comment.author = request.user
+#             new_comment.save()
+#             form = CommentForm()
+#             return redirect("product_list")
+#     else:
+#         form = CommentForm()
+#
+#     return render(request, "products/product_detail.html", {"product": product,
+#                                                             "comments": comments,
+#                                                             "form": form,
+#                                                             "fee": fee})
