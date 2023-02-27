9d977252986ae28ef4ecf11fb191c56660d539c4
mohammadrezaelahi <m.reza.elahi.1378@gmail.com>
2023-02-28 00:50:27 +0330

    complete add to cart

--- cart/urls.py
+++ cart/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = "cart"
 
 urlpatterns = [
-    path("", views.cart_detail_view, name="cart_detail")
+    path("", views.cart_detail_view, name="cart_detail"),
+    path("add/<int:product_id>", views.add_to_cart_view, name="cart_add"),
 ]
