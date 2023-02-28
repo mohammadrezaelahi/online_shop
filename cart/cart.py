8346133cea166faa5d46a15225880717953d491c
mohammadrezaelahi <m.reza.elahi.1378@gmail.com>
2023-02-28 10:21:58 +0330

    inplace or add items to cart

--- cart/cart.py
+++ cart/cart.py
@@ -15,11 +15,13 @@ class Cart:
 
         self.cart = cart
 
-    def add(self, product, quantity=1):
+    def add(self, product, quantity=1, replace=False):
         product_id = str(product.id)
 
         if product_id not in self.cart:
-            self.cart[product_id] = {"quantity": quantity}
+            self.cart[product_id] = {"quantity": 0}
+        if replace:
+            self.cart[product_id]["quantity"] = quantity
         else:
             self.cart[product_id]["quantity"] += quantity
 
