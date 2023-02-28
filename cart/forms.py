e5117861d0233d72a07a77636a40423eb29db9aa
mohammadrezaelahi <m.reza.elahi.1378@gmail.com>
2023-02-28 10:24:14 +0330

    inplace or add items to cart

--- cart/forms.py
+++ cart/forms.py
@@ -5,3 +5,4 @@ class AddToCardProductForm(forms.Form):
     QUANTITY_CHOISES = [(i, str(i)) for i in range(1, 30)]
 
     quantity = forms.TypedChoiceField(choices=QUANTITY_CHOISES, coerce=int)
+    inplace = forms.BooleanField(required=False, widget=forms.HiddenInput)
