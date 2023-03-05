1b91c8ebd865c1253c38cd2fd34994aa18239459
mohammadrezaelahi <m.reza.elahi.1378@gmail.com>
2023-03-05 22:39:03 +0330

    add short_description & verbose_name in Product

--- products/models.py
+++ products/models.py
@@ -8,14 +8,15 @@ from ckeditor.fields import RichTextField
 
 
 class Product(models.Model):
-    title = models.CharField(max_length=100)
-    description = RichTextField()
-    price = models.PositiveIntegerField()
-    active = models.BooleanField(default=True)
+    title = models.CharField(verbose_name=_("title"), max_length=100)
+    description = RichTextField(verbose_name=_("description"))
+    short_description = models.CharField(verbose_name=_("short description"), max_length=400)
+    price = models.PositiveIntegerField(verbose_name=_("price"))
+    active = models.BooleanField(verbose_name=_("active"), default=True)
     image = models.ImageField(verbose_name=_("product image"), upload_to="product/product_cover/", blank=True)
 
-    datetime_created = models.DateTimeField(default=timezone.now)
-    datetime_modified = models.DateTimeField(auto_now=True)
+    datetime_created = models.DateTimeField(verbose_name=_("date created"), default=timezone.now)
+    datetime_modified = models.DateTimeField(verbose_name=_("date modified"), auto_now=True)
 
     def __str__(self):
         return self.title
