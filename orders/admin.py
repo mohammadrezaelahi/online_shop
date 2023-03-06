d7d04845fb5c724eca8e5931149285939f5c2d66
mohammadrezaelahi <m.reza.elahi.1378@gmail.com>
2023-03-06 23:51:56 +0330

    admin panel for orders

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from jalali_date.admin import ModelAdminJalaliMixin
 
-# Register your models here.
+from .models import Order, OrderItem
+
+
+class OrderItemsInline(admin.TabularInline):
+    model = OrderItem
+    fields = ["order", "product", "quantity", "price", ]
+    extra = 0
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ["user", "is_paid", "first_name", "last_name", "datetime_created"]
+    inlines = [
+        OrderItemsInline,
+    ]
+
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ["order", "product", "quantity", "price", ]
