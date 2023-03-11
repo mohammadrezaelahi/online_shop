bb637608823109b13a2cf79c9cca040ad3f21049
mohammadrezaelahi <m.reza.elahi.1378@gmail.com>
2023-03-11 13:54:40 +0330

    work by forms in template

--- orders/views.py
+++ orders/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
 
+from .forms import OrderForm
+
 
 def order_create_view(request):
-    return render(request, "orders/order_create.html")
+    order_form = OrderForm()
+
+    if request.method == "POST":
+        order_form = OrderForm(request.POST)
+
+        if order_form.is_valid():
+            order_obj = order_form.save(commit=False)
+            order_obj.user = request.user
+            order_obj.save()
+
+    return render(request, "orders/order_create.html", {
+        "form": order_form,
+    })
